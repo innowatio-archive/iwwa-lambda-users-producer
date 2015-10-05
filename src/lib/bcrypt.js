1942aa40a686a088ee80874f5bc374cf625a884c
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-10-05 14:42:28 +0200

    Fixed bug: inverted parameters in bcrypt compare

--- src/lib/bcrypt.js
+++ src/lib/bcrypt.js
@@ -4,6 +4,6 @@ export function hash (password) {
     return bcrypt.hashSync(password, 10);
 }
 
-export function compare (hash, password) {
+export function compare (password, hash) {
     return bcrypt.compareSync(password, hash);
 }
